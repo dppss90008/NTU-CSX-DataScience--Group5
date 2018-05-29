ca9d52ca33a783ab66546ccec7f6a87a35302c23
dppss90008 <34399929+dppss90008@users.noreply.github.com>
2018-05-29 20:30:54 +0800

    Xie

--- FaceBookAPI.R
+++ FaceBookAPI.R
@@ -2,25 +2,43 @@ library(jsonlite)
 library(httr)
 library(magrittr)
 
-# Crawl data from facebookAPI
-token = "EAACEdEose0cBAJAFfop9CXDM7djByDqTvPClh9aziKXpqYWegeaYMOocDRzTCfNs14CJDjpNwUR6MtBmn2AXkFk5moZALVnCTeZAk63iRXa74Cyvg7b3wa2QSEN1a0zuhaXBeSCFEocZA8mpMBZCM1YeIydy8GzuVPTZAPW6CB9Qd4h2kXjG2AnLnon6iwogZD"
+####六都候選人臉書粉絲專頁####
+
+##台北市##
+#柯文哲https://www.facebook.com/DoctorKoWJ/
+#姚文智https://www.facebook.com/YaoTurningTaipei/
+#丁守中https://www.facebook.com/tingshouchung/
+
+##新北市##
+
+##台中市##
+
+##測試使用##
+#enc.teia
+
+##############################
+
+# Crawl meassage data from facebookAPI
+token = "EAACEdEose0cBAKpgRr3xJTS8ONSZA7xyZB0y2YX3PHPl7tJWrZAzPXuKqKZCxRE911bWb9L4nUSl25UUS5kjGlzz4ZBcau5CPVa5GqK48hFwbllsWhOtZCZAI1j0DZCpM3aAK4OcnDWzhnT62HQYUPkZBktIVEanTgPIzQlzBaVAPDZAIdEi8ng3XvRMkTouic8VsZD"
 FacebookID = "enc.teia"
 url_1 = "https://graph.facebook.com/v2.12/"
 url_2 = "?fields=posts%7Bcomments.limit(20)%7D&access_token="
 url = paste0(url_1,FacebookID,url_2,token)
 response = GET(url)
-posts  = content(response)
+message  = content(response)
 
-
-data <- posts$posts$data
-
-# �P�_�O�_���H�d��
-
-ismessage = sapply(posts$posts$data,function(x){
+# Get message from post <List>
+message <- message$posts$data
+  
+# find if there is empty post(no messafe return 0)
+ismessage = sapply(message,function(x){
   if(length(x)==2) return (1)
   else return (0)
 })
 
+ismessage
+
+# Get post idx(have message)
 ismessageidx = NULL
 for(i in c(1:length(ismessage))){
   if(ismessage[i]==1)
@@ -28,13 +46,70 @@ for(i in c(1:length(ismessage))){
 }
 
 ismessageidx
-Output = data.frame()
-for(i in ismessageidx){
-  dataIn <- posts$posts$data[[i]]$comments$data %>% do.call(rbind,.) %>% data.frame()
-  Output = rbind(Output,dataIn)
+
+## create dataframe with same col number
+## there is a from in posts$posts$data[[2]]$comments$data[[1]]$from
+
+
+findMesNum <- function(i){
+  message[[i]]$comments$data %>% length %>% return()
 }
-data1 <- posts$posts$data[[5]]$comments$data %>% do.call(rbind,.) %>% data.frame()
-data2 <- posts$posts$data[[6]]$comments$data %>% do.call(rbind,.) %>% data.frame()
-data3 <- posts$posts$data[[9]]$comments$data %>% do.call(rbind,.) %>% data.frame()
-View(rbind(data1,data2,data3))
+findMes2Num <- function(x,y){
+  message[[x]]$comments$data[[y]] %>% length %>% return()
+}
+
+for(i in ismessageidx){
+  for(j in c(1:findMesNum(i))){
+    if(findMes2Num(i,j)==4){
+      message[[i]]$comments$data[[j]]$from <- NULL
+    }
+  }
+}
+
+Output <- data.frame()
+for(i in ismessageidx){
+  data <- message[[i]]$comments$data %>% do.call(rbind,.)%>% data.frame()
+  data <- cbind(i,data)
+  Output <- rbind(Output,data)  
+}
+
+##########################################################################
+
+# Crawl Posts data from facebookAPI
+url_1 = "https://graph.facebook.com/v3.0/"
+url_2 = "?fields=posts.limit(25)&access_token="
+url = paste0(url_1,FacebookID,url_2,token)
+response = GET(url)
+Posts  = content(response)
+
+# Get posts/message from post <List>
+Posts <- Posts$posts$data
+
+# Get post create time
+ismessageidx
+
+Time <- c()
+for(i in ismessageidx){
+  Time <- c(Time,Posts[[i]]$created_time)
+}
+Time
+
+
+##################################################################
+# get shares from every post
+url_1 = "https://graph.facebook.com/v3.0/"
+url_2 = "?fields=posts.limit(25){shares}&access_token="
+url = paste0(url_1,FacebookID,url_2,token)
+response = GET(url)
+shares  = content(response)
+shares <- shares$posts$data
+shares[[1]]$shares[[1]]
+
+shareCT <- c()
+for(i in c(1:25)){
+  shareCT <- c(shareCT,shares[[i]]$shares[[1]])
+}
+shareCT
+
+
 
