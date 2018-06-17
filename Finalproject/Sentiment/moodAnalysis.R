d5c79457dd098ee4a380af3bc21e5e581fa45e4d
dppss90008 <34399929+dppss90008@users.noreply.github.com>
2018-06-17 20:50:35 +0800

    Xie

--- Finalproject/Sentiment/moodAnalysis.R
+++ Finalproject/Sentiment/moodAnalysis.R
@@ -1,60 +1,51 @@
 library(stringr)
 library(jiebaR)
+library(jiebaRD)
 
-
-
-
-
-pos<-read.csv('NTUSD_positive_UTF-8.csv',header=F,stringsAsFactors=FALSE,encoding = "unicode")
-weight <- rep(1, length(pos[,1])) #正面情感词语赋权重为1
+## 使用情感字典 <NTUSD>
+pos<-read.csv('NTUSD_positive_unicode.csv',header=F,stringsAsFactors=FALSE,encoding = "unicode")
+weight <- rep(1, length(pos[,1])) #正面情感詞語權重為1
 pos <- cbind(pos, weight)
-neg<-read.csv('NTUSD_negative_UTF-8.csv',header=F,stringsAsFactors=FALSE,encoding = "unicode")
-weight <- rep(-1, length(neg[,1])) #负面情感词语赋权重为-1
+neg<-read.csv('NTUSD_negative_unicode.csv',header=F,stringsAsFactors=FALSE,encoding = "unicode")
+weight <- rep(-1, length(neg[,1])) #負面情感詞語權重為-1
 neg <- cbind(neg, weight)
 posneg<-rbind(pos,neg)
 colnames(posneg)<-c('term','weight')
 
+# 建立切分詞字典及環境
 user<-posneg[,'term']
-
-Data <- read.csv("Di_JanFebNews.csv")
-text <- Data$V3[1]
-text
 w1<-worker()
 new_user_word(w1,user)
+
+
+Data <- read.csv("Di_JanFebNews.csv")
+
+
 
 
 # 文字清理
 library(NLP)
 library(tm)
-library(jiebaRD)
-library(jiebaR)
 
 docs <- Corpus(VectorSource(Data$V3))
 toSpace <- content_transformer(function(x,pattern){
   return(gsub(pattern," ",x))
 })
 
-docs <- tm_map(docs,toSpace,"\n")
-docs <- tm_map(docs,toSpace, "[A-Za-z0-9]")
-docs <- tm_map(docs, removePunctuation)
-docs <- tm_map(docs, removeNumbers)
-docs <- tm_map(docs, stripWhitespace)
-docs <- tm_map(docs, toSpace, "、")
-docs <- tm_map(docs, toSpace, "，")
-docs <- tm_map(docs, toSpace, "。")
-docs <- tm_map(docs, toSpace, "！")
-docs <- tm_map(docs, toSpace, "「")
-docs <- tm_map(docs, toSpace, "（")
-docs <- tm_map(docs, toSpace, "」")
-docs <- tm_map(docs, toSpace, "）")
-docs <- tm_map(docs, toSpace, "\n")
-docs <- tm_map(docs, toSpace, "；")
-docs <- tm_map(docs, removeNumbers)
-docs <- tm_map(docs, toSpace, "[a-zA-Z]")
-docs <- tm_map(docs, stripWhitespace)
+clean_doc <- function(docs){
+  clean_words <- c("[A-Za-z0-9]","、","，","。","！","「","（","」","）","\n","；")
+  for(i in 1:length(clean_words)){
+    docs <- tm_map(docs,toSpace, clean_words[i])
+  }
+  return(docs)
+}
 
+docs <- clean_doc(docs)
+
+
+# 開始切詞及計算情感分數
 text <- docs[[1]]$content
-text<-list(text)
+
 res<-segment(unlist(text),w1)
 res
 temp<-data.frame()
@@ -73,8 +64,6 @@ Ct_pos <- temp[temp$weight==1,3] %>% length()
 Ct_neg <- temp[temp$weight==-1,3] %>% length()
 
 Ct_pos/(Ct_pos+Ct_neg)
-
-res
 
 
 # word freq
