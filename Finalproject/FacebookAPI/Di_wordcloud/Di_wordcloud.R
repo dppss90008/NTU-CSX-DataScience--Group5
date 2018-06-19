7eb7ea24cb2b5f3134ede64815297b6b3f38c47e
Weber12321 <36941261+Weber12321@users.noreply.github.com>
2018-06-19 22:15:22 +0800

    weber

--- Finalproject/FacebookAPI/Di_wordcloud/Di_wordcloud.R
+++ Finalproject/FacebookAPI/Di_wordcloud/Di_wordcloud.R
@@ -3,7 +3,10 @@
 # ------------------------------------------
 # 
 # 匯入套件
+library(tibble)
 library(dplyr)
+library(tidyr)
+library(data.table)
 library(NLP)
 library(tm)
 library(jiebaRD)
@@ -14,7 +17,26 @@ library(wordcloud)
 
 # 匯入資料組
 setwd("/Users/Weber/Documents/GitHub/NTU-CSX-DataScience--Group5/Finalproject/FacebookAPI")
-Di_data <- read.csv("Di_report.csv")
+data <- read.csv("Di_report.csv")
+# 清除NA
+data <- data %>% na.omit()
+# 切開時間
+data <- data %>% separate(time, c("year","month","day"),"-")
+data <- data %>% separate(day, c("date","time"), "T")
+# 排序
+data <- data[with(data, order(year ,month, date)), ]
+# 清理重複資料
+data <- data[!duplicated(data$post), ]
+# 移除2017資料
+data <- data[data$year == "2018",]
+row.names(data) = c(1:260) # 由資料數重新編排號碼
+# 依月份建立子資料組
+Di1<- subset(data , data$month == "01", select = post)
+Di2<- subset(data , data$month == "02", select = post)
+Di3<- subset(data , data$month == "03", select = post)
+Di4<- subset(data , data$month == "04", select = post)
+Di5<- subset(data , data$month == "05", select = post)
+Di6<- subset(data , data$month == "06", select = post)
 # Ko_data <- read.csv("Ko_report.csv")
 # Yao_data <- read.csv("Yao_report.csv")
 
@@ -33,7 +55,7 @@ Di_data <- read.csv("Di_report.csv")
 
 # ==== 丁守中
 # ---- Jan
-docs <- Corpus(VectorSource(Di_data$post[214:266]))
+docs <- Corpus(VectorSource(Di1$post))
 toSpace <- content_transformer(function(x,pattern){
   return(gsub(pattern," ",x))
 })
@@ -49,7 +71,7 @@ clean_doc <- function(docs){
 }
 docs <- clean_doc(docs)
 clean_word_doc <- function(docs){
-  clean_words <- c("我們","他們","的","也","都","就","與","但","是","在","和","及","為","或","且","有","含")
+  clean_words <- c("分享","記者","攝影","提及","表示","報導","我們","他們","的","也","都","就","與","但","是","在","和","及","為","或","且","有","含")
   for(i in 1:length(clean_words)){
     docs <- tm_map(docs,toSpace, clean_words[i])
   }
@@ -69,6 +91,7 @@ new_user_word(mixseg,segment)
 jieba_tokenizer=function(d){
   unlist(segment(d[[1]],mixseg))
 }
+
 seg = lapply(docs, jieba_tokenizer)
 freqFrame = as.data.frame(table(unlist(seg)))
 # 清除單字
@@ -80,19 +103,20 @@ for(i in c(1:length(freqFrame$Var1))){
 freqFrame <- na.omit(freqFrame)
 # 畫出文字雲
 # 儲存文字雲圖片
-png("Di_Jan.png", width = 500, height = 500 )
+png("Di_Jan.png", width = 300, height = 300)
 
 wordcloud(freqFrame$Var1,freqFrame$Freq,
-          min.freq=10,
+          min.freq=15,
           random.order=TRUE,random.color=TRUE, 
           rot.per=.1, colors=rainbow(length(row.names(freqFrame))),
           ordered.colors=FALSE,use.r.layout=FALSE,
           fixed.asp=TRUE)
+
 dev.off()
 
 
 # ---- Feb
-docs <- Corpus(VectorSource(Di_data$post[163:213]))
+docs <- Corpus(VectorSource(Di2$post))
 toSpace <- content_transformer(function(x,pattern){
   return(gsub(pattern," ",x))
 })
@@ -108,7 +132,7 @@ clean_doc <- function(docs){
 }
 docs <- clean_doc(docs)
 clean_word_doc <- function(docs){
-  clean_words <- c("我們","他們","的","也","都","就","與","但","是","在","和","及","為","或","且","有","含")
+  clean_words <- c("分享","記者","攝影","提及","表示","報導","我們","他們","的","也","都","就","與","但","是","在","和","及","為","或","且","有","含")
   for(i in 1:length(clean_words)){
     docs <- tm_map(docs,toSpace, clean_words[i])
   }
@@ -139,10 +163,10 @@ for(i in c(1:length(freqFrame$Var1))){
 freqFrame <- na.omit(freqFrame)
 # 畫出文字雲
 # 儲存文字雲圖片
-png("Di_Feb.png", width = 500, height = 500 )
+png("Di_Feb.png", width = 300, height = 300 )
 
 wordcloud(freqFrame$Var1,freqFrame$Freq,
-          min.freq=10,
+          min.freq=15,
           random.order=TRUE,random.color=TRUE, 
           rot.per=.1, colors=rainbow(length(row.names(freqFrame))),
           ordered.colors=FALSE,use.r.layout=FALSE,
@@ -151,7 +175,7 @@ dev.off()
 
 
 # ---- Mar
-docs <- Corpus(VectorSource(Di_data$post[115:162]))
+docs <- Corpus(VectorSource(Di3$post))
 toSpace <- content_transformer(function(x,pattern){
   return(gsub(pattern," ",x))
 })
@@ -167,7 +191,7 @@ clean_doc <- function(docs){
 }
 docs <- clean_doc(docs)
 clean_word_doc <- function(docs){
-  clean_words <- c("我們","他們","的","也","都","就","與","但","是","在","和","及","為","或","且","有","含")
+  clean_words <- c("分享","記者","攝影","提及","表示","報導","我們","他們","的","也","都","就","與","但","是","在","和","及","為","或","且","有","含")
   for(i in 1:length(clean_words)){
     docs <- tm_map(docs,toSpace, clean_words[i])
   }
@@ -198,10 +222,10 @@ for(i in c(1:length(freqFrame$Var1))){
 freqFrame <- na.omit(freqFrame)
 # 畫出文字雲
 # 儲存文字雲圖片
-png("Di_Mar.png", width = 500, height = 500 )
+png("Di_Mar.png", width = 300, height = 300 )
 
 wordcloud(freqFrame$Var1,freqFrame$Freq,
-          min.freq=10,
+          min.freq=15,
           random.order=TRUE,random.color=TRUE, 
           rot.per=.1, colors=rainbow(length(row.names(freqFrame))),
           ordered.colors=FALSE,use.r.layout=FALSE,
@@ -210,7 +234,7 @@ dev.off()
 
 
 # ---- Apr
-docs <- Corpus(VectorSource(Di_data$post[62:114]))
+docs <- Corpus(VectorSource(Di4$post))
 toSpace <- content_transformer(function(x,pattern){
   return(gsub(pattern," ",x))
 })
@@ -226,7 +250,7 @@ clean_doc <- function(docs){
 }
 docs <- clean_doc(docs)
 clean_word_doc <- function(docs){
-  clean_words <- c("我們","他們","的","也","都","就","與","但","是","在","和","及","為","或","且","有","含")
+  clean_words <- c("分享","記者","攝影","提及","表示","報導","我們","他們","的","也","都","就","與","但","是","在","和","及","為","或","且","有","含")
   for(i in 1:length(clean_words)){
     docs <- tm_map(docs,toSpace, clean_words[i])
   }
@@ -257,10 +281,10 @@ for(i in c(1:length(freqFrame$Var1))){
 freqFrame <- na.omit(freqFrame)
 # 畫出文字雲
 # 儲存文字雲圖片
-png("Di_Apr.png", width = 500, height = 500 )
+png("Di_Apr.png", width = 300, height = 300 )
 
 wordcloud(freqFrame$Var1,freqFrame$Freq,
-          min.freq=10,
+          min.freq=15,
           random.order=TRUE,random.color=TRUE, 
           rot.per=.1, colors=rainbow(length(row.names(freqFrame))),
           ordered.colors=FALSE,use.r.layout=FALSE,
@@ -270,7 +294,7 @@ dev.off()
 
 
 # ---- May
-docs <- Corpus(VectorSource(Di_data$post[20:61]))
+docs <- Corpus(VectorSource(Di5$post))
 toSpace <- content_transformer(function(x,pattern){
   return(gsub(pattern," ",x))
 })
@@ -286,7 +310,7 @@ clean_doc <- function(docs){
 }
 docs <- clean_doc(docs)
 clean_word_doc <- function(docs){
-  clean_words <- c("我們","他們","的","也","都","就","與","但","是","在","和","及","為","或","且","有","含")
+  clean_words <- c("分享","記者","攝影","提及","表示","報導","我們","他們","的","也","都","就","與","但","是","在","和","及","為","或","且","有","含")
   for(i in 1:length(clean_words)){
     docs <- tm_map(docs,toSpace, clean_words[i])
   }
@@ -317,7 +341,7 @@ for(i in c(1:length(freqFrame$Var1))){
 freqFrame <- na.omit(freqFrame)
 # 畫出文字雲
 # 儲存文字雲圖片
-png("Di_May.png", width = 500, height = 500 )
+png("Di_May.png", width = 300, height = 300 )
 
 wordcloud(freqFrame$Var1,freqFrame$Freq,
           min.freq=10,
@@ -326,3 +350,130 @@ wordcloud(freqFrame$Var1,freqFrame$Freq,
           ordered.colors=FALSE,use.r.layout=FALSE,
           fixed.asp=TRUE)
 dev.off()
+
+
+# ---- Jun
+docs <- Corpus(VectorSource(Di6$post))
+toSpace <- content_transformer(function(x,pattern){
+  return(gsub(pattern," ",x))
+})
+# 刪去單詞贅字、英文字母、標點符號、數字與空格
+docs <- tm_map(docs,toSpace,"\n")
+docs <- tm_map(docs,toSpace, "[A-Za-z0-9]")
+clean_doc <- function(docs){
+  clean_words <- c("[A-Za-z0-9]","、","《","『","』","【","】","／","，","。","！","「","（","」","）","\n","；",">","<","＜","＞")
+  for(i in 1:length(clean_words)){
+    docs <- tm_map(docs,toSpace, clean_words[i])
+  }
+  return(docs)
+}
+docs <- clean_doc(docs)
+clean_word_doc <- function(docs){
+  clean_words <- c("分享","記者","攝影","提及","表示","報導","我們","他們","的","也","都","就","與","但","是","在","和","及","為","或","且","有","含")
+  for(i in 1:length(clean_words)){
+    docs <- tm_map(docs,toSpace, clean_words[i])
+  }
+  return(docs)
+}
+docs <- clean_word_doc(docs)
+docs <- tm_map(docs, removeNumbers)
+docs <- tm_map(docs, toSpace, "[a-zA-Z]")
+docs <- tm_map(docs, stripWhitespace)
+docs <- tm_map(docs, removePunctuation)
+# 匯入自定義字典
+mixseg = worker()
+segment <- c("柯文哲","姚文智","丁守中","台北市長","選舉","候選人","台灣","選票","柯市長","民進黨","國民黨","台北市民","市民")
+new_user_word(mixseg,segment)
+
+# 有詞頻之後就可以去畫文字雲
+jieba_tokenizer=function(d){
+  unlist(segment(d[[1]],mixseg))
+}
+seg = lapply(docs, jieba_tokenizer)
+freqFrame = as.data.frame(table(unlist(seg)))
+# 清除單字
+for(i in c(1:length(freqFrame$Var1))){
+  if((freqFrame$Var1[i] %>% as.character %>% nchar) == 1){
+    freqFrame[i,] <- NA
+  }
+}
+freqFrame <- na.omit(freqFrame)
+# 畫出文字雲
+# 儲存文字雲圖片
+png("Di_Jun.png", width = 300, height = 300 )
+
+wordcloud(freqFrame$Var1,freqFrame$Freq,
+          min.freq=5,
+          random.order=TRUE,random.color=TRUE, 
+          rot.per=.1, colors=rainbow(length(row.names(freqFrame))),
+          ordered.colors=FALSE,use.r.layout=FALSE,
+          fixed.asp=TRUE)
+dev.off()
+
+# ---- 2018
+docs <- Corpus(VectorSource(data$post))
+toSpace <- content_transformer(function(x,pattern){
+  return(gsub(pattern," ",x))
+})
+# 刪去單詞贅字、英文字母、標點符號、數字與空格
+docs <- tm_map(docs,toSpace,"\n")
+docs <- tm_map(docs,toSpace, "[A-Za-z0-9]")
+clean_doc <- function(docs){
+  clean_words <- c("[A-Za-z0-9]","、","《","『","』","【","】","／","，","。","！","「","（","」","）","\n","；",">","<","＜","＞")
+  for(i in 1:length(clean_words)){
+    docs <- tm_map(docs,toSpace, clean_words[i])
+  }
+  return(docs)
+}
+docs <- clean_doc(docs)
+clean_word_doc <- function(docs){
+  clean_words <- c("分享","記者","攝影","提及","表示","報導","我們","他們","的","也","都","就","與","但","是","在","和","及","為","或","且","有","含")
+  for(i in 1:length(clean_words)){
+    docs <- tm_map(docs,toSpace, clean_words[i])
+  }
+  return(docs)
+}
+docs <- clean_word_doc(docs)
+docs <- tm_map(docs, removeNumbers)
+docs <- tm_map(docs, toSpace, "[a-zA-Z]")
+docs <- tm_map(docs, stripWhitespace)
+docs <- tm_map(docs, removePunctuation)
+# 匯入自定義字典
+mixseg = worker()
+segment <- c("柯文哲","姚文智","丁守中","台北市長","選舉","候選人","台灣","選票","柯市長","民進黨","國民黨","台北市民","市民")
+new_user_word(mixseg,segment)
+
+# 有詞頻之後就可以去畫文字雲
+jieba_tokenizer=function(d){
+  unlist(segment(d[[1]],mixseg))
+}
+seg = lapply(docs, jieba_tokenizer)
+freqFrame = as.data.frame(table(unlist(seg)))
+# 清除單字
+for(i in c(1:length(freqFrame$Var1))){
+  if((freqFrame$Var1[i] %>% as.character %>% nchar) == 1){
+    freqFrame[i,] <- NA
+  }
+}
+freqFrame <- na.omit(freqFrame)
+# 畫出文字雲
+# 儲存文字雲圖片
+png("Di_2018.png", width = 400, height = 400 )
+
+wordcloud(freqFrame$Var1,freqFrame$Freq,
+          min.freq=500,
+          random.order=TRUE,random.color=TRUE, 
+          rot.per=.1, colors=rainbow(length(row.names(freqFrame))),
+          ordered.colors=FALSE,use.r.layout=FALSE,
+          fixed.asp=TRUE)
+dev.off()
+
+
+# 報導量
+Di_textNum <- rbind(nrow(Di1),nrow(Di2),nrow(Di3),nrow(Di4),nrow(Di5),nrow(Di6) )
+Di_textNum %>% as.data.frame()
+colnames(Di_textNum) <- "丁守中報導量"
+rownames(Di_textNum) <- c("Jan","Feb","Mar","Apr","May","Jun")
+Di_textNum
+# 輸出
+write.table(Di_textNum, file = "Di_textNum.CSV", sep = ",")
