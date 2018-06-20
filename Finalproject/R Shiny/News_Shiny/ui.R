ad8312d54913ed5c18e223783db03ee2f4f441d1
dppss90008 <34399929+dppss90008@users.noreply.github.com>
2018-06-21 07:24:25 +0800

    XDD

--- Finalproject/R Shiny/News_Shiny/ui.R
+++ Finalproject/R Shiny/News_Shiny/ui.R
@@ -9,10 +9,10 @@ shinyUI(navbarPage("選戰懶人包",
                                 sidebarPanel(
                                   
                                   # Input: Choose dataset ----
-                                  selectInput("Candi", "Choose a dataset:",
+                                  selectInput("Candi", "北市候選人",
                                               choices = list("柯文哲"="柯文哲", "丁守中"="丁守中", "姚文智"="姚文智","全部"=4))
                                   ,checkboxInput("line", label = "加上趨勢線", value = FALSE),
-                                  radioButtons("mood", label = h3("Radio buttons"),
+                                  radioButtons("mood", label = h4("臉書表情"),
                                                choices = c("like","share","angry","sad","haha","love","wow","sentiment"), 
                                                selected = "like")
                                   
@@ -34,7 +34,7 @@ shinyUI(navbarPage("選戰懶人包",
                               tabPanel("盒狀圖",sidebarLayout(
                                 sidebarPanel(
                                   
-                                  radioButtons("mood2", label = h4("Radio buttons"),
+                                  radioButtons("mood2", label = h4("臉書表情"),
                                                choices = c("like","share","angry","sad","haha","love","wow","sentiment"),
                                                selected = "like"),
                                   checkboxInput("outlier", label = "去除極端值", value = FALSE)
@@ -50,12 +50,12 @@ shinyUI(navbarPage("選戰懶人包",
                                        sidebarLayout(
                                          sidebarPanel(
                                            
-                                           selectInput("Candi2", "Choose a dataset:",
+                                           selectInput("Candi2", "北市候選人",
                                                        choices = c("柯文哲", "丁守中", "姚文智"))
                                            ,numericInput('shows', '顯示項目', 5,
                                                          min = 1, max = 50),
                                            checkboxInput("decrease", label = "從大排到小", value = TRUE),
-                                           radioButtons("mood3", label = h4("Radio buttons"),
+                                           radioButtons("mood3", label = h4("臉書表情"),
                                                         choices = c("like","share","angry","sad","haha","love","wow","sentiment"),
                                                         selected = "like")
                                            
@@ -70,15 +70,41 @@ shinyUI(navbarPage("選戰懶人包",
                                          
                                          
                                        )),
-                              tabPanel("文字雲"),
+                              tabPanel("臉書文字雲",
+                                       sidebarLayout(
+                                         sidebarPanel(
+                                           selectInput("candiXD", "北市候選人 :",
+                                                       choices = list("柯文哲" = 1, "丁守中" = 2, "姚文智" = 3),
+                                                       selected = 1),
+                                           radioButtons("yue", label = h3("月份"),
+                                                        choices = list("2018" = 0, "Jan" = 1, "Feb" = 2, "Mar" = 3, "Apr" = 4, "May" = 5, "Jun" = 6), 
+                                                        selected = 0)
+                                           
+                                           
+                                         ),
+                                         mainPanel(
+                                           
+                                           h3("候選人粉專文字雲"),
+                                           plotOutput("FaceCloudPlot", height = 600, width = 600)
+                                           
+                                         )
+                                         
+                                         
+                                         
+                                         
+                                         
+                                         
+                                       )
+                                       
+                              ),
                               tabPanel("LDA",sidebarLayout(
                                 sidebarPanel(
-                                  selectInput("PostCandi", "Choose a dataset:",
+                                  selectInput("PostCandi", "北市候選人",
                                               choices = c("柯文哲", "丁守中", "姚文智"))
                                   
                                 ),mainPanel(
                                   
-                                  h4("Observations"),
+                                  h4("LDA Result"),
                                   plotOutput("postLDA",height = 600, width = 1200)
                                   
                                 )
@@ -95,12 +121,12 @@ shinyUI(navbarPage("選戰懶人包",
                               tabPanel("情緒分析<以候選人分類>",
                                        sidebarLayout(
                                          sidebarPanel(
-                                           selectInput("newsCandi", "Choose a dataset:",
+                                           selectInput("newsCandi", "北市候選人",
                                                        choices = c("柯文哲", "丁守中", "姚文智"))
                                            
                                          ),mainPanel(
                                            
-                                           h4("Observations"),
+                                           h4("情緒分析<以候選人分類>"),
                                            plotOutput("NewsMood")
                                            
                                          )
@@ -113,12 +139,12 @@ shinyUI(navbarPage("選戰懶人包",
                              tabPanel("情緒分析<以報紙分類>",
                                       sidebarLayout(
                                         sidebarPanel(
-                                          selectInput("newsName", "Choose a dataset:",
+                                          selectInput("newsName", "四大媒體",
                                                       choices = c("UDN", "CT", "LTN","Apple"))
                                           
                                         ),mainPanel(
                                           
-                                          h4("Observations"),
+                                          h4("情緒分析<以報紙分類>"),
                                           plotOutput("NewsMood2")
                                           
                                         )
@@ -127,19 +153,48 @@ shinyUI(navbarPage("選戰懶人包",
                                       
                                       
                              ),
-                              tabPanel("文字雲"),
+                              tabPanel("四大報文字雲",
+                                       sidebarLayout(
+                                         sidebarPanel(
+                                           selectInput("CandiXD", "北市候選人 :",
+                                                       choices = list("柯文哲" = 1, "丁守中" = 2, "姚文智" = 3),
+                                                       selected = 1),
+                                           radioButtons("news", label = h3("媒體與社群"),
+                                                        choices = list("自由" = 1, "聯合" = 2, "中時" = 3, "蘋果" = 4),
+                                                        selected = 1),
+                                           radioButtons("month", label = h3("月份"),
+                                                        choices = list("2018" = 0, "Jan" = 1, "Feb" = 2, "Mar" = 3, "Apr" = 4, "May" = 5), 
+                                                        selected = 0)
+                                           
+                                           
+                                         ),
+                                         mainPanel(
+                                           
+                                           h3("候選人文字雲"),
+                                           plotOutput("CloudPlot", height = 700, width = 700)
+                                           
+                                         )
+                                         
+                                         
+                                         
+                                         
+                                         
+                                         
+                                       )
+                                       
+                              ),
                               tabPanel("LDA" ,
                                        
                                        sidebarLayout(
                                           sidebarPanel(
-                                            selectInput("newsLDA", "Choose a dataset:",
+                                            selectInput("newsLDA", "四大媒體",
                                               choices = c("UDN", "CT", "LTN","Apple")),
-                                            selectInput("nameLDA", "Choose a dataset:",
+                                            selectInput("nameLDA", "北市候選人",
                                               choices = c("柯文哲", "丁守中", "姚文智"))
                                   
                                         ),mainPanel(
                                   
-                                              h4("Observations"),
+                                              h4("LDA Result"),
                                               plotOutput("newsLDA",height = 600, width = 1200)
                                   
                                 )
